67e5a2d4e832f80bedffadecd287f9aa0bf53a80
viseth89 <viseth89@gmail.com>
2019-01-31 21:24:25 -0600

    created first commit

---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h4>John Doe</h4>
+        <li>Email: jdoe@gmail.com</li>
+        <li>Phone: 555-555-5555</li>
+      </div>
+    )
+  }
+}
+
+
+export default Contact;
+
